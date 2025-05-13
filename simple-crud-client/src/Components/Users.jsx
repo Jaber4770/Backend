d7af804cc928edff7b1bb9c88d490a267d41cf4d
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-05-13 18:51:58 +0200

    create read and delete done

--- simple-crud-client/src/Components/Users.jsx
+++ simple-crud-client/src/Components/Users.jsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { use, useState } from "react";
 
-const Users = () => {
+const Users = ({ userPromise }) => {
+    const userData = use(userPromise);
+    console.log(userData);
+    const [userInfo, setUserInfo] = useState(userData);
+    console.log(userInfo);
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = { name, email };
+        console.log(user);
+
+
+        // create user:
+        fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    console.log(data);
+                    user._id = data.insetId;
+                    const newAddedUser = [...userInfo, user];
+                    setUserInfo(newAddedUser);
+                    e.target.reset();
+                }
+            })
+    }
+
+    const handleUserDelete = (id) => { 
+        console.log('delete', id);
+        fetch(`http://localhost:3000/users/${id}`, {
+            method:'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount) {
+                    const remainingUsers = userInfo.filter(user => user._id != id);
+                    setUserInfo(remainingUsers);
+                    console.log(data)
+                }
+            });
+    }
+
+
     return (
         <div>
-            
+            <div>
+                <form onSubmit={handleOnSubmit}>
+                    <label htmlFor="">Name</label>
+                    <input name="name" type="text" placeholder='name' /><br />
+                    <label htmlFor="">Email</label>
+                    <input name="email" type="email" placeholder='email' />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+            <div>
+                {
+                    userInfo.map(u => {
+                        return (<p
+                            key={u._id}>
+                            {u.name}:
+                            {u.email}
+                            <button onClick={() => handleUserDelete(u._id)}>X</button>
+                        </p>)
+                    })
+                }
+            </div>
         </div>
     );
 };
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient,ServerApiVersion} = require("mongodb");
+const { MongoClient,ServerApiVersion, ObjectId} = require("mongodb");
 const port = process.env.PORT || 3000;
 
 // middleware
@@ -24,6 +24,33 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+
+        const database = client.db("userdb");
+        const usersCollection = database.collection("users");
+
+
+        app.get('/users', async(req, res) => {
+            const cursor = usersCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.post('/users', async(req, res) => {
+            console.log("data in the server: ", req.body);
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.delete(`/users/:id`, async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You seccessfully connected to MongoDB!");
     }
